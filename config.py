64a5354bd5e696261d3b4b66b1175ea44f6be506
ANnn <100271317+Ayahdjc1@users.noreply.github.com>
2025-06-01 22:37:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # API-токен Telegram-бота
-API_TOKEN = '7520962720:AAHwJ1XyNc9M81ALbUWM4W7k7anPNT1Z_D0'  # Замените на ваш собственный API токен, полученный через BotFather
+API_TOKEN = ''  # Замените на ваш собственный API токен, полученный через BotFather
 # Добавить:
-ADMIN_IDS = [1077073462]
+ADMIN_IDS = []
 # Параметры базы данных
 DB_PATH = 'engagement_data.db'  # Путь к базе данных SQLite
 
